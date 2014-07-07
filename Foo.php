7da8ebdb194103256f609043b759421cca0ad069
alaminaji <alamin_santoso@yahoo.com>
2014-07-07 22:10:53 +0700

    Create Foo.php

---
+++ Foo.php
@@ -0,0 +1,18 @@
+<?php
+
+  echo "Github";
+  $var = "Git";
+  echo $var; //this is comment
+  
+  $arr = array("satu", "dua", "tiga", "empat", "lima");
+  foreach($arr as $prnt)
+  {
+    echo $prnt . "<br>";
+  }
+  
+  if(print_r($arr))
+  {
+    //this is weird
+  }
+
+?>
