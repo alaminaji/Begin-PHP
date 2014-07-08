a3feedd51d3c49c41eaceab3387f4e2e738b0bc0
alaminaji <alamin_santoso@yahoo.com>
2014-07-08 16:56:13 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
+//no purpose to create this program just practicing with c++
+//written by alaminaji
 #include <iostream>
-#include <string>
 using namespace std;
+
+void printResult( char nam[], int number[]);
 
 int main()
 {
-  cout <<"Cout";
+  char name[50];
+  int numb[10];
+  
+  cout <<"Silahkan masukkan Nama dan Nomor telfon( pisahkan dengan spasi atau enter):";
+  cin >> name >> numb;
+  printResult(name, numb);
+  
+  system("PAUSE");
   return 0;
+}
+
+void printResult( char nam[], int number[])
+{
+  if(number[3] && number[4] == 8)
+  {
+    cout <<"Selamat anda telah mengisi ulang nomor **** anda.\n";
+    cout <<"Datang kembali"<<nam;
+  }
 }
