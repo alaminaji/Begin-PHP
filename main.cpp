d388e1ec8c9c6889c0ddca426bc67083ad14ac2f
alaminaji <alamin_santoso@yahoo.com>
2014-07-08 18:35:51 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,23 +15,23 @@ struct chocoJuice {
 int main()
 {
   int cho, wat, pri, sumi = 0;  //variable for storing input
-  chocoJuice *product;    //instance for product
+  chocoJuice product;          //instance for product
   
   cout <<"How much Chocolate price:";
   cin >>cho;
-  product->choco = cho;
+  product.choco = cho;
   cout <<"How much Mineral Water price:";
   cin >>wat;
-  product->water = wat;
+  product.water = wat;
   cout <<"How much price one cup of ChocoJuice:";
   cin >>pri;
-  product->price = pri;
-  product->sum = pri * wat * cho; //getting sum value of one chocoJuice
+  product.price = pri;
+  product.sum = pri * wat * cho; //getting sum value of one chocoJuice
   cout <<"How much do you want to produce:"
   cin >>sumi;
   
   cout <<"\n\n";
-  cout <<"The total of your product is:"<<sumi * product->sum<<" \n\n"; //getting the total product
+  cout <<"The total of your product is:"<<sumi * product.sum<<" \n\n"; //getting the total product
   system("PAUSE");
   return 0;
 }
