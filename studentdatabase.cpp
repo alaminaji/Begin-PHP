af0f77fc66f6be58ee3a2f77a4e5db2eb945cd2d
alaminaji <alamin_santoso@yahoo.com>
2014-07-08 19:46:03 +0700

    Update studentdatabase.cpp

--- studentdatabase.cpp
+++ studentdatabase.cpp
@@ -3,10 +3,17 @@
 //Student Database System
 #include <iostream>
 #include <string>
+#include <fstream>
 using namespace std;
 
 int main()
 {
-  
+  ofstream myfile;
+	myfile.open ("example.txt");
+	myfile <<"Student\t\t\tCourse.\n";
+	myfile <<"=======\t\t\t======\n"; 
+	myfile <<"Alamien" << "\t\t\t" << "IT\n";
+	myfile.close();
+
   return 0;
 }
