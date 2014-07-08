57044a54ae7a47effeef9af3933d2cbe13adc446
alaminaji <alamin_santoso@yahoo.com>
2014-07-08 18:26:05 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,28 +1,37 @@
-//no purpose to create this program just practicing with c++
-//written by alaminaji
+//no purpose to create this program just practicing with c++.
+//written by alaminaji.
+//Product Selling Price.
+//program compile yet.
 #include <iostream>
 using namespace std;
 
-void printResult( char nam[], int number[]);
+struct chocoJuice {
+  int choco;
+  int water;
+  int price;
+  int sum;
+};
 
 int main()
 {
-  char name[50];
-  int numb[10];
+  int cho, wat, pri, sumi = 0;  //variable for storing input
+  chocoJuice *product;    //instance for product
   
-  cout <<"Silahkan masukkan Nama dan Nomor telfon( pisahkan dengan spasi atau enter):";
-  cin >> name >> numb;
-  printResult(name, numb);
+  cout <<"How much Chocolate price:";
+  cin >>cho;
+  product->choco = cho;
+  cout <<"How much Mineral Water price:";
+  cin >>wat;
+  product->water = wat;
+  cout <<"How much price one cup of ChocoJuice:";
+  cin >>pri;
+  product->price = pri;
+  product->sum = pri * wat * cho; //getting sum value of one chocoJuice
+  cout <<"How much do you want to produce:"
+  cin >>sumi;
   
+  cout <<"\n\n";
+  cout <<"The total of your product is:"<<sumi * product->sum<<" \n\n"; //getting the total product
   system("PAUSE");
   return 0;
-}
-
-void printResult( char nam[], int number[])
-{
-  if(number[3] && number[4] == 8)
-  {
-    cout <<"Selamat anda telah mengisi ulang nomor **** anda.\n";
-    cout <<"Datang kembali"<<nam;
-  }
 }
